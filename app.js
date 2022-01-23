0e6d3e462c8267d36848ca6803e06d4b137cdb76
shruti <shru.kharkar2916@gmail.com>
2022-01-23 18:30:45 -0500

    Server using Express

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var message = 'Hello World';
 console.log(message); //or console.log('Hello World');
 
 //Simple nodeJS server
-var http = require('http');
+/*var http = require('http');
 var server = http.createServer(function(req, res){
     if((req.url == '/')){
         res.write('<html><body><p>Hello World. This is System Integration Class!!</p></body></html>');
@@ -10,4 +10,19 @@ var server = http.createServer(function(req, res){
 })
 
 server.listen(3000);
-console.log('Server is running in port 3000');
+console.log('Server is running in port 3000');*/
+
+//NodeJS server using Express
+
+const express = require('express');
+const app = express();
+let port=3000;
+let host = 'localhost';
+app.listen(port,host, () => {
+    console.log('Server is running in port', port);
+})
+
+app.get('/', (req,res)=>
+{
+    res.send('Hello World. This is System Integration Class!!!')
+});
