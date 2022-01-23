c47919e1831d1a0a4ec613ae44e0c6ca5f0aa398
shruti <shru.kharkar2916@gmail.com>
2022-01-23 18:22:49 -0500

    Simple nodeJS server

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var message = 'Hello World';
 console.log(message); //or console.log('Hello World');
+
+//Simple nodeJS server
+var http = require('http');
+var server = http.createServer(function(req, res){
+    if((req.url == '/')){
+        res.write('<html><body><p>Hello World. This is System Integration Class!!</p></body></html>');
+    }
+})
+
+server.listen(3000);
+console.log('Server is running in port 3000');
